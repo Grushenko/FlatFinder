d47b168a327aa25ea5d98f0d5e35e966fa41a6aa
Wojciech Gruszka <wojciech@gruszka.eu>
2016-09-08 21:08:16 +0200

    Added files required by OpenShift

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='FlatFinder',
+      version='0.1.0',
+      description='Flat finder scheduler',
+      author='Wojciech Gruszka',
+      author_email='wojciech@gruszka.eu',
+      url='http://www.python.org/sigs/distutils-sig/',
+#      install_requires=['Django>=1.3'],
+     )
