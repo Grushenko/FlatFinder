f40930ad315ea8472db37cb2d12d18392688e363
Wojciech Gruszka <wojciech@gruszka.eu>
2016-09-09 00:38:56 +0200

    Added printing found offers via template system

--- FlatFinder.py
+++ FlatFinder.py
@@ -1,27 +1,12 @@
 import os
-
+from jinja import Environment, FileSystemLoader
+env = Environment(loader=FileSystemLoader('html'))
 
 def wsgi(environ, start_response):
-    ctype = 'text/plain'
-    if environ['PATH_INFO'] == '/health':
-        response_body = "1"
-    elif environ['PATH_INFO'] == '/env':
-        response_body = ['%s: %s' % (key, value)
-                         for key, value in sorted(environ.items())]
-        response_body = '\n'.join(response_body)
-    else:
-        ctype = 'text/html'
-        response_body = ''
+        tmpl = env.get_template('index.html')
         found = ''
-        with open('html/index.html', 'r') as html:
-            response_body = html.read()
         with open(os.environ['OPENSHIFT_DATA_DIR']+'found.txt', 'r') as html:
             found = html.read()
+        found = found.split('\n')
+        return tmpl.render(found=found)
 
-        response_body.replace('{{body}}', found)
-
-    status = '200 OK'
-    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
-    #
-    start_response(status, response_headers)
-    return [response_body]
