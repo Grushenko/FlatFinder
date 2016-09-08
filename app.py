9a6dd1433dccfdc67577189330757c66f564f863
Wojciech Gruszka <wojciech@gruszka.eu>
2016-09-08 22:16:06 +0200

    Fixing encoding

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 import os
 import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 import wsgi
 import finder
 import threading
@@ -18,6 +20,7 @@ except IOError:
 ip = os.environ['OPENSHIFT_PYTHON_IP']
 port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
 host_name = os.environ['OPENSHIFT_GEAR_DNS']
+
 
 t = threading.Thread(target=finder.Finder('config', os.environ['MX_USER'], os.environ['MX_PASSWORD']).run)
 t.daemon = True
