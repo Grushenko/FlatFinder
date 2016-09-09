1a2613d16393ac5f1730af418bdcd0d9b28ce670
Wojciech Gruszka <wojciech@gruszka.eu>
2016-09-09 18:35:28 +0200

    changed credentials loading

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python
-
+import codecs
 import os
 import sys
 
-import GumTreeFinder
-import OLXFinder
-from FinderThread import FinderThread
+import ConfigParser
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
-import FlatFinder
 import cherrypy
 from cherrypy import wsgiserver
 
-OLX = None
-GumTree = None
+from GumTreeFinder import GumTreeFinder
+from OLXFinder import OLXFinder
+from FinderThread import FinderThread
+import FlatFinder
 
-if os.environ.has_key('OPENSHIFT_PYTHON_IP'):
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+# OPENSHIFT
+if 'OPENSHIFT_PYTHON_IP' in os.environ:
     sys.path.insert(0, os.path.dirname(__file__))
     virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
     virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
@@ -29,24 +30,31 @@ if os.environ.has_key('OPENSHIFT_PYTHON_IP'):
     port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
     host_name = os.environ['OPENSHIFT_GEAR_DNS']
     data_dir = os.environ['OPENSHIFT_DATA_DIR']
-    GumTree = GumTreeFinder.GumTreeFinder(data_dir, 'config_gumtree', os.environ['MX_USER'], os.environ['MX_PASSWORD'])
-   # OLX = OLXFinder.OLXFinder(data_dir, 'config_olx', os.environ['MX_USER'], os.environ['MX_PASSWORD'])
-
-
 else:
-    ip = '127.0.0.1'
-    port = 8080
-    host_name = 'localhost'
-    data_dir = './data/'
-    GumTree = GumTreeFinder.GumTreeFinder(data_dir, 'config_gumtree')
-#    OLX = OLXFinder.OLXFinder(data_dir, 'config_olx')
+    if len(sys.argv) == 3:
+        ip = sys.argv[1]
+        port = sys.argv[2]
+        host_name = sys.argv[1] + ':' + sys.argv[2]
+        data_dir = '../data'
+    else:
+        ip = '127.0.0.1'
+        port = 8080
+        host_name = 'localhost'
+        data_dir = './data/'
 
-#OLXThread = FinderThread(OLX)
+cred_conf = ConfigParser.RawConfigParser()
+cred_conf.readfp(codecs.open(data_dir+'credentials', 'r', 'utf-8'))
+mx_user = cred_conf.get('mx', 'mx_user')
+mx_password = cred_conf.get('mx', 'mx_password')
+
+GumTree = GumTreeFinder(data_dir, 'config_gumtree', mx_user, mx_password)
 GumTreeThread = FinderThread(GumTree)
-
-#OLXThread.start()
 GumTreeThread.start()
 
-wsgiapp = cherrypy.Application(FlatFinder.FlatFinder(data_dir, {'olx': None, 'gumtree': GumTreeThread}), '/')
-server = wsgiserver.CherryPyWSGIServer((ip, port), wsgiapp, server_name=host_name)
+OLX = OLXFinder(data_dir, 'config_olx', mx_user, mx_password)
+OLXThread = FinderThread(OLX)
+OLXThread.start()
+
+wsgi_app = cherrypy.Application(FlatFinder.FlatFinder(data_dir, {'olx': OLXThread, 'gumtree': GumTreeThread}), '/')
+server = wsgiserver.CherryPyWSGIServer((ip, port), wsgi_app, server_name=host_name)
 server.start()
