54a0f30a7ca9424240eabb4a43c6342fc0deb8fb
Wojciech Gruszka <wojciech@gruszka.eu>
2016-09-09 15:40:45 +0200

    Fancier logging

--- GumTreeFinder.py
+++ GumTreeFinder.py
@@ -5,6 +5,7 @@ class GumTreeFinder(Finder):
     def __init__(self, data_dir, config_file, mx_user=None, mx_password=None):
         super(GumTreeFinder, self).__init__(data_dir, config_file, mx_user, mx_password)
         self.subject = 'GumTree Offers: Mieszkania w Warszawie'
+        self.log_file = 'found_gumtree.txt'
 
 if __name__ == "__main__":
     GumTreeFinder('./data/', 'config_gumtree').run()
